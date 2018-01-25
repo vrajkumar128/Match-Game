9b15d2506e63f5bb6800a8fce1c02d34027ad0c4
vrrajkum <vivek.rajkumar@protonmail.com>
2018-01-25 18:04:34 -0500

    Write JavaScript to render a random board.

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -1,16 +1,31 @@
-var MatchGame = {};
+const MatchGame = {};
 
 /*
   Sets up a new game after HTML document has loaded.
   Renders a 4x4 board of cards.
 */
 
+$(document).ready(function() {
+  MatchGame.renderCards(MatchGame.generateCardValues(), $('#game'));
+});
+
 /*
   Generates and returns an array of matching card values.
  */
 
 MatchGame.generateCardValues = function () {
-
+  const cardValues = [];
+  for (let value = 1; value < 9; value++) {
+    cardValues.push(value);
+    cardValues.push(value);
+  }
+  const randomValues = [];
+  while (cardValues.length > 0) {
+    const randomIndex = Math.floor(Math.random() * cardValues.length);
+    randomValues.push(cardValues[randomIndex]);
+    cardValues.splice(randomIndex, 1);
+  }
+  return randomValues;
 };
 
 /*
@@ -19,7 +34,25 @@ MatchGame.generateCardValues = function () {
 */
 
 MatchGame.renderCards = function(cardValues, $game) {
-
+  $game.empty();
+  const cardColors = ["hsl(25, 85%, 65%)",
+  "hsl(55, 85%, 65%)",
+  "hsl(90, 85%, 65%)",
+  "hsl(160, 85%, 65%)",
+  "hsl(220, 85%, 65%)",
+  "hsl(265, 85%, 65%)",
+  "hsl(310, 85%, 65%)",
+  "hsl(360, 85%, 65%)"]
+  cardValues.forEach(function(value) {
+    const $card = $('<div class="col-xs-3 card">' + value + '</div>');
+    const data = {
+      value: value,
+      color: cardColors[value - 1],
+      flipped: false
+    }
+    $card.data(data);
+    $game.append($card);
+  })
 };
 
 /*
